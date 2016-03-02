58ce6ecab8f04a5a7d0763f9f691eabd47d7188f
ferram4 <ferram40@gmail.com>
2016-03-02 14:21:09 -0500

    More settings options

--- MouseAimFlight/MouseAimSettings.cs
+++ MouseAimFlight/MouseAimSettings.cs
@@ -59,6 +59,48 @@ namespace MouseAimFlight
             }
         }
 
+        static float mouseSensitivity = 100;
+        public static float MouseSensitivity
+        {
+            get { return mouseSensitivity; }
+            set
+            {
+                if (value != mouseSensitivity)
+                {
+                    mouseSensitivity = value;
+                    Instance.SaveSettings();
+                }
+            }
+        }
+
+        static bool invertY = false;
+        static bool invertX = false;
+        public static bool InvertYAxis
+        {
+            get { return invertY; }
+            set
+            {
+                if (value != invertY)
+                {
+                    invertY = value;
+                    Instance.SaveSettings();
+                }
+            }
+        }
+
+        public static bool InvertXAxis
+        {
+            get { return invertX; }
+            set
+            {
+                if (value != invertX)
+                {
+                    invertX = value;
+                    Instance.SaveSettings();
+                }
+            }
+        }
+
         MouseAimSettings()
         {
             LoadSettings();
@@ -76,7 +118,19 @@ namespace MouseAimFlight
                 }
                 if(node.HasValue("cursorStyle"))
                 {
-                    cursor = (CursorStyle)Enum.Parse(typeof(CursorStyle), (string)node.GetValue("cursorStyle"));
+                    Enum.TryParse<CursorStyle>((string)node.GetValue("cursorStyle"), out cursor);
+                }
+                if (node.HasValue("mouseSensitivity"))
+                {
+                    float.TryParse(node.GetValue("mouseSensitivity"), out mouseSensitivity);
+                }
+                if(node.HasValue("invertX"))
+                {
+                    bool.TryParse(node.GetValue("invertX"), out invertX);
+                }
+                if (node.HasValue("invertY"))
+                {
+                    bool.TryParse(node.GetValue("invertY"), out invertX);
                 }
             }
         }
@@ -87,6 +141,9 @@ namespace MouseAimFlight
             node.AddValue("name", "default");
             node.AddValue("toggleKey", toggleKeyCode.ToString());
             node.AddValue("cursorStyle", cursor.ToString());
+            node.AddValue("mouseSensitivity", mouseSensitivity.ToString());
+            node.AddValue("invertX", invertX.ToString());
+            node.AddValue("invertY", invertY.ToString());
 
             ConfigNode saveNode = new ConfigNode();
             saveNode.AddNode(node);
